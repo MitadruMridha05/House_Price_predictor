db4a4417b7938d0d26b9659272a29db0029a6115
MitadruMridha05 <24me01059@iitbbs.ac.in>
2025-05-15 09:01:08 +0530

    Add .gitignore file

--- mlflow.py
+++
@@ -1,7 +0,0 @@
-import dagshub
-dagshub.init(repo_owner='MitadruMridha05', repo_name='House_Price_predictor', mlflow=True)
-
-import mlflow
-with mlflow.start_run():
-  mlflow.log_param('parameter name', 'value')
-  mlflow.log_metric('metric name', 1)
